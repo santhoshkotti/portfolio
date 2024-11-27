0d951485450584db35bb060920b6a64619d33ddf
santhoshkotti <santhoshkotti561.com>
2024-11-27 15:40:31 +0530

    Done SEO and changed Icon

--- src/app/page.js
+++ src/app/page.js
@@ -5,19 +5,40 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
+import { NextSeo } from 'next-seo';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
+    <>
+      <NextSeo
+        title="Santhoshkumar P - Web Developer & Designer"
+        description="Explore the portfolio of Santhoshkumar P, showcasing projects, skills, and achievements in web development and design."
+        openGraph={{
+          type: 'website',
+          url: 'https://www.yourwebsite.com',
+          title: 'Santhoshkumar P - Web Developer & Designer',
+          description: 'Explore the portfolio of Santhoshkumar P.',
+          images: [
+            {
+              url: '/path/to/image.jpg',
+              width: 800,
+              height: 600,
+              alt: 'Portfolio Image Alt Text',
+            },
+          ],
+        }}
+      />
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Navbar />
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          {/* <AchievementsSection /> */}
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
